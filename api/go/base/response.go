ddd9786db3713cf07809c3296328a8f1621c39f6
AdityaP1502 <i.m.aditya015@gmail.com>
2023-11-20 15:40:37 +0700

    Update base response struct

--- api/go/base/response.go
+++ api/go/base/response.go
@@ -3,7 +3,8 @@ package base
 type DataField map[string]interface{} // struct to hold key-value pairs for json response
 
 type Response struct {
-	Status  string    `json:"status"`
-	Message string    `json:"message"`
-	Data    DataField `json:"data,omitempty"`
+	Status     string    `json:"status"`
+	Message    string    `json:"message"`
+	Data       DataField `json:"data,omitempty"`
+	StatusCode int       `json:"-"`
 }
