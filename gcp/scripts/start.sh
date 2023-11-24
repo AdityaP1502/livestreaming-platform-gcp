c384187ae3f719e3b367dcd0ecdeecbabb58c7b8
Aditya Putra <73848290+AdityaP1502@users.noreply.github.com>
2023-11-25 06:02:16 +0700

    Update start.sh
    save the ip field from response to file

--- gcp/scripts/start.sh
+++ gcp/scripts/start.sh
@@ -4,6 +4,9 @@
 echo $1
 STREAM_LINK="rtsp://$VM_IP:8554/$1"
 STORAGE_LINK="$1"
+TMP_DIR="/usr/local/stream/tmp/$STORAGE_LINK"
+
+mkdir -p $TMP_DIR
 
 JSON_STRING=$( jq -n \
         --arg sl "$STREAM_LINK" \
@@ -14,4 +17,4 @@ JSON_STRING=$( jq -n \
 echo "$JSON_STRING" > file.json
 echo $TRANSCODER_LB_IP
 echo $JSON_STRING
-curl -X POST -H "Content-Type: application/json"  -d "$JSON_STRING" $TRANSCODER_LB_IP:8080/init < /dev/null
+curl -X POST -H "Content-Type: application/json"  -d "$JSON_STRING" $TRANSCODER_LB_IP:8080/init | jq '.[].ip' > $TMP_DIR/ip
