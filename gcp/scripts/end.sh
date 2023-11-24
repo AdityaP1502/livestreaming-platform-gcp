6e21f783ebad250498a84e1fe26e9839dc614d06
Aditya Putra <73848290+AdityaP1502@users.noreply.github.com>
2023-11-25 06:04:13 +0700

    Update end.sh
    Send end request to the ip in file

--- gcp/scripts/end.sh
+++ gcp/scripts/end.sh
@@ -2,13 +2,13 @@
 echo $1
 STREAM_LINK="rtsp://$VM_IP:8554/$1"
 STORAGE_LINK="$1"
+TMP_DIR="/usr/local/stream/tmp/$STORAGE_LINK"
 
 JSON_STRING=$( jq -n \
         --arg sl "$STREAM_LINK" \
         --arg stl "$STORAGE_LINK" \
         '{"stream-link": $sl, "storage-link": $stl}' )
 
+TRANSCODER_IP=$(cat $TMP_DIR/ip)
 
-echo $TRANSCODER_LB_IP
-echo $JSON_STRING
-curl -X POST -H "Content-Type: application/json"  -d "$JSON_STRING" $TRANSCODER_LB_IP:8080/end < /dev/null
+curl -X POST -H "Content-Type: application/json"  -d "$JSON_STRING" $TRANSCODER_IP:8080/end > /dev/null
